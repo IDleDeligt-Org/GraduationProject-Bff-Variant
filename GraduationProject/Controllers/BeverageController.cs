7d29c4840359f679067f415053d9c1d88d784640
Shamonzen <simonpaquindaigle@hotmail.com>
2023-04-11 15:34:37 +0200

    Simon made the Controller content's first draft. This needs reviewing and testing.

---
+++ GraduationProject/Controllers/BeverageController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GraduationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraduationProject.Controllers
+{
+    [Route("api/Beverage")]
+    [ApiController]
+    public class BeverageController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BeverageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Beverage>> GetBeverages()
+        {
+            return _context.Beverages.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Beverage> GetBeverage(int id)
+        {
+            Beverage? beverage = _context.Beverages.Find(id);
+
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+
+            return beverage;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutBeverage(int id, Beverage beverage)
+        {
+            if (id != beverage.BeverageId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(beverage).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<Beverage> PostBeverage(Beverage beverage)
+        {
+            _context.Beverages.Add(beverage);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetBeverage), new { id = beverage.BeverageId }, beverage);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBeverage(int id)
+        {
+            Beverage? beverage = _context.Beverages.Find(id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+
+            _context.Beverages.Remove(beverage);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
+
---
+++ GraduationProject/Controllers/IngredientController.cs
@@ -0,0 +1,78 @@
+﻿// Controllers/IngredientController.cs
+using System.Collections.Generic;
+using System.Linq;
+using GraduationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraduationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IngredientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Ingredient>> GetIngredients()
+        {
+            return _context.Ingredients.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Ingredient> GetIngredient(int id)
+        {
+            var ingredient = _context.Ingredients.Find(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return ingredient;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutIngredient(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.IngredientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ingredient).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<Ingredient> PostIngredient(Ingredient ingredient)
+        {
+            _context.Ingredients.Add(ingredient);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetIngredient), new { id = ingredient.IngredientId }, ingredient);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteIngredient(int id)
+        {
+            var ingredient = _context.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ingredients.Remove(ingredient);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
